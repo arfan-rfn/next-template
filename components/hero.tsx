8ee92dd51b59b5d5e120f823d97d5e5f1b91e5e2
arfan-rfn <arfan.rfn@gmail.com>
2024-05-23 00:22:17 -0700

    added a dot to make it little fancy

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,12 @@
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import DotPattern from "./dot-pattern";
+import { cn } from "@/lib/utils";
 
 export function Hero() {
 	return (
-		<section className="container grid flex-1 items-center gap-6 pb-8 pt-6 md:py-10">
+		<section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
 			<div className="flex max-w-[980px] flex-col items-start gap-2">
 				<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
 					Beautifully designed components <br className="hidden sm:inline" />
@@ -33,6 +35,11 @@ export function Hero() {
 					GitHub
 				</Link>
 			</div>
+			<DotPattern
+				className={cn(
+					"[mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)] ",
+				)}
+			/>
 		</section>
 	);
 }
