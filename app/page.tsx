72faec557d34f29a4498e1992f8c37246d59285d
arfan-rfn <arfan.rfn@gmail.com>
2024-05-22 21:47:20 -0700

    Added v1 SEO on the template

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import { Footer } from "@/components/footer"
 import { Hero } from "@/components/hero"
+import { JsonLd } from "@/components/json-ld";
+import { getSEOTags } from "@/lib/seo";
+
+export const metadata = getSEOTags({
+  title: "NextJS Template",
+  description: "A NextJS template with Tailwind CSS and TypeScript.",
+  relativeUrl: "/",
+}
+);
+
 
 export default function IndexPage() {
+
+  const jsonLd = {
+    name: "NextJS Template",
+    description: "A NextJS template with Tailwind CSS and TypeScript.",
+    relativeUrl: "/",
+  }
+
   return (
     <>
+      <JsonLd data={jsonLd} />
       <main className="h-screen">
         <Hero />
       </main>
---
+++ components/json-ld.tsx
@@ -0,0 +1,30 @@
+
+type SchemaType = {
+	data: {
+		name: string;
+		description: string;
+		relativeUrl: string;
+		imageUrl?: string;
+		[key: string]: any;
+	}
+}
+
+export function JsonLd({ data }: SchemaType) {
+	const { imageUrl, relativeUrl } = data;
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	const fullUrl = `${baseUrl}${relativeUrl}`;
+	const jsonLd = {
+		"@context": "http://schema.org",
+		"@type": "WebPage",
+		"url": fullUrl,
+		"image": imageUrl,
+		...data,
+	};
+
+	return (
+		<script
+			type="application/ld+json"
+			dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+		/>
+	);
+}
