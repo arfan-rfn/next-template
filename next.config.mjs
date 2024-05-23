4ec540df579777b82a2ca0f41b6166c0c5cf7f6b
arfan-rfn <arfan.rfn@gmail.com>
2024-05-23 03:04:15 -0700

    file cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,3 @@
-// import { fileURLToPath } from "node:url";
-// import createJiti from "jiti";
-// const jiti = createJiti(fileURLToPath(import.meta.url));
-
-// jiti("./env");
 import "./env.mjs"
 
 /** @type {import('next').NextConfig} */
