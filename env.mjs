a0cb4a041952524ee53b3a394b1b369892474607
arfan-rfn <arfan.rfn@gmail.com>
2024-05-24 18:22:54 -0700

    added mongo client

--- env.mjs
+++ env.mjs
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
 	server: {
 		NODE_ENV: z.enum(["development", "test", "production"]),
+		MONGODB_URI: z.string().min(1),
 	},
 	client: {
 		NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: z.string().min(1),
