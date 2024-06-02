c4fc5630fb6720736ce18a0616cb6cf2c459f7cc
arfan-rfn <arfan.rfn@gmail.com>
2024-06-02 14:39:21 -0700

    added aws-s3 config

---
+++ app/api/s3/[key]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getS3Object, getS3SrcByKey } from "../get-image-by-key";
+
+async function _s3StreamResponse(key: string) {
+	const { Body, ContentType } = await getS3Object(key);
+
+	if (!Body || !ContentType) {
+		return NextResponse.json({ error: "Image not found" }, { status: 404 });
+	}
+
+	const response = new NextResponse(Body as BodyInit);
+	response.headers.set("Content-Type", ContentType);
+	return response;
+}
+
+type GetOptions = {
+	params: {
+		key: string;
+	};
+}
+
+export async function GET(_: NextRequest, { params }: GetOptions) {
+	// return s3StreamResponse(params.key);
+	const src = await getS3SrcByKey(params.key, {
+		expiresIn: 60 * 60,
+	});
+	return NextResponse.json({ src });
+}
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import "./env.mjs"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{
+      hostname: "*.amazonaws.com"
+    }],
+  },
 }
 
 export default nextConfig
