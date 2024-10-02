c17c7f939d22203cd634471b93dd0896a4046052
arfan-rfn <arfan.rfn@gmail.com>
2024-10-02 08:04:08 -0700

    fix env error on client side

--- components/query-provider.tsx
+++ components/query-provider.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { env } from '@/env.mjs'
 import { getQueryClient } from '@/lib/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -12,7 +11,7 @@ export default function QueryProviders({ children }: { children: React.ReactNode
 	return (
 		<QueryClientProvider client={queryClient}>
 			{children}
-			{env.NODE_ENV !== "production" && <ReactQueryDevtools />}
+			{process.env.NODE_ENV !== "production" && <ReactQueryDevtools />}
 		</QueryClientProvider>
 	)
 }
