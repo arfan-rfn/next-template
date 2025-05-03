b5311daa44f2a62df67862b32e34456e200ae804
arfan-rfn <arfan.rfn@gmail.com>
2025-05-03 16:59:13 -0700

    updated shadcn to latest

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,26 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function toTitleCase(str: string) {
-  return str.replace(
-    /\w\S*/g,
-    (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
-  );
-}
-
-export function toSentenceCase(str: string) {
-  return str
-    .replace(/([A-Z])/g, " $1")
-    .replace(/^./, (str) => str.toUpperCase());
-}
-
-export function getBaseURL() {
-  if (process.env.NODE_ENV === 'development') {
-    return '';
-  }
-  return env.NEXT_PUBLIC_BASE_URL;
-}
-
 export function toAbsoluteUrl(url: string): string {
   if (!url) return "";
 
